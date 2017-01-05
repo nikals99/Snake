6cfd59d07d3215469e8bf12241c225238142c2af
niklas fassbender <niklas.fassbender@t-online.de>
2017-01-05 15:07:03 +0100

    Added JUnit Tests

---
+++ test/sample/models/SnakeTest.java
@@ -0,0 +1,26 @@
+package sample.models;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Created by niklas.fassbender on 05.01.2017.
+ */
+public class SnakeTest {
+
+    @Test
+    public void testExtendEmptySnake() {
+        //given
+        Snake snake = new Snake();
+
+        //when
+        snake.extend();
+
+        //then
+        Assert.assertEquals("Leere extendete Schlange soll Länge 1 haben",1, snake.getPositions().size());
+
+    }
+
+
+
+}
